b6a69c4087c5a43a5fe9de38c1a5f7bf09ae7d32
Roberto <rleisguerrero@gmail.com>
2020-03-31 09:45:48 -0300

    Remove reset.css and refactoring

--- js/main.js
+++ js/main.js
@@ -1,39 +1,34 @@
 import inputValidation from './validation.js';
-import {convertToDecimal, convertFromDecimal} from './conversion.js';
+import { convertToDecimal, convertFromDecimal } from './conversion.js';
 
-const convertButton = document.querySelector(".container__button");
+const convertButton = document.querySelector('.container__button');
 
-function newOutputTr (value){
+function newOutputTr(value) {
+  if (!document.querySelector('.container__output')) {
+    const outputTd = document.createElement('td');
+    const outputTr = document.createElement('tr');
+    const tbody = document.querySelector('.container__tbody');
 
-    if(!document.querySelector(".container__output")){
-        
-        const outputTd = document.createElement("td");
-        const outputTr = document.createElement("tr");
-        const tbody = document.querySelector(".container__tbody");
+    outputTd.classList.add('container__output');
+    outputTd.setAttribute('colspan', '2');
+    outputTd.textContent = value;
 
-        outputTd.classList.add("container__output");
-        outputTd.setAttribute("colspan","2");
-        outputTd.textContent = value;
-
-        outputTr.appendChild(outputTd);
-        tbody.children[0].insertAdjacentElement("afterend", outputTr);
-
-    }else{
-
-        document.querySelector(".container__output").textContent = value;
-    }    
+    outputTr.appendChild(outputTd);
+    tbody.children[0].insertAdjacentElement('afterend', outputTr);
+  } else {
+    document.querySelector('.container__output').textContent = value;
+  }
 }
-convertButton.addEventListener("click", ()=>{
-    const inputType = document.querySelector("#inputType").value;
-    const outputType = document.querySelector("#outputType").value;
-    let inputValue = document.querySelector(".container__input").value;
+convertButton.addEventListener('click', () => {
+  const inputType = document.querySelector('#inputType').value;
+  const outputType = document.querySelector('#outputType').value;
+  let inputValue = document.querySelector('.container__input').value;
 
-    if(!inputValidation(inputType, inputValue)){
-        alert("Invalid!");
-    }else{
-        
-        const outputValue = convertFromDecimal(convertToDecimal(inputValue, inputType), outputType);
-        newOutputTr(outputValue);
-        document.querySelector(".container__input").value = "";
-    }
-})
+  if (!inputValidation(inputType, inputValue)) {
+    alert('Invalid!');
+  } else {
+    const outputValue = convertFromDecimal(convertToDecimal(inputValue, inputType), outputType);
+    newOutputTr(outputValue);
+    document.querySelector('.container__input').value = '';
+  }
+});
--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,19 @@
 export default inputValidation;
 
-function returnBaseRegExp (base){
-
-    switch(base){
-        case '10':
-            return new RegExp("[0-9]");
-        case '2':
-            return new RegExp("[01]");
-        case '16':
-            return new RegExp("[0-9A-Fa-f]");
-        case '8':
-            return new RegExp("[0-7]");
-    }
+function returnBaseRegExp(base) {
+  switch (base) {
+    case '10':
+      return new RegExp('[0-9]');
+    case '2':
+      return new RegExp('[01]');
+    case '16':
+      return new RegExp('[0-9A-Fa-f]');
+    case '8':
+      return new RegExp('[0-7]');
+  }
 }
-
-function inputValidation (inputType, input){
-    
-    let isValid = true;
-
-    [...input].forEach((number)=>{
-        if(!returnBaseRegExp(inputType).test(number)){
-            return isValid = false;
-        }
-    })
-
-    return isValid;
+function inputValidation(inputType, input) {
+  return [...input].every(number => {
+    return !returnBaseRegExp(inputType).test(number);
+  });
 }
